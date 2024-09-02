ae999879b5786ce97675491525ab130fe7e45048
MariaKar1991 <mariaskaravazaki@gmail.com>
2024-09-02 13:24:09 +0100

    changes in apiKey.js, adding comments

--- apiKey.js
+++ apiKey.js
@@ -1,3 +1,7 @@
+// Allowing the apiKey.js file to be used in github in order to deploy the app.
+//In a professional setting
+//the api key should be secured(preferably in the backend
+//and not shared in a public repository.
 const apiKey = 'AIzaSyCLLDRZugPbQ_tw6uXVMpmpIfraiyclroc'
 
 export default apiKey
